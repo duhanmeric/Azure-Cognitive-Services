1d47ef51951f9903514b68f14b168008c22a67d4
Meriç <75217465+duhanmeric@users.noreply.github.com>
2022-04-03 20:18:01 +0300

    deploy

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post("/audio", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
